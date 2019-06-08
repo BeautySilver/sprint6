91820dab19b05b6aaeb18644fc6e3523d391a568
BeautySilver <gubar1234567@gmail.com>
2019-06-08 16:22:52 +0300

    Assync

--- first_app.js
+++ first_app.js
@@ -1,6 +1,24 @@
-
 const http = require("http");
  
+function display(data, callback){
+ 
+    
+    let randInt = Math.random() * (10 - 1) + 1;
+    let err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
+     
+    setTimeout(function(){
+        callback(err, data);
+    }, 0);
+}
+  console.log("Начало работы программы");
+
+displaySync("Обработка данных...",function (err, data){
+ 
+    if(err) throw err;
+    console.log(data);
+});
+
+
 let message = "Hello World!";
 http.createServer(function(request,response){
      
@@ -10,3 +28,9 @@ http.createServer(function(request,response){
 }).listen(3000, "127.0.0.1",()=>{
     console.log("Сервер начал прослушивание запросов");
 });
+
+function displaySync(data){
+    console.log(data);
+}
+  
+console.log("Завершение работы программы");
