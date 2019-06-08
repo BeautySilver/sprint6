837f03da7ddac01f27d4871f00ba70aefb1320fe
BeautySilver <gubar1234567@gmail.com>
2019-06-08 16:48:34 +0300

    Finish Read files

--- first_app.js
+++ first_app.js
@@ -1,18 +1,33 @@
-function displaySync(callback){
-    callback();
-}
+const fs = require("fs");
  
-console.log("Начало работы программы");
+fs.writeFile("text.txt", "Hello user!", function(error){
  
-setTimeout(function(){
-         
-        console.log("timeout 500");
-}, 500);
+    if(error) throw error; 
+    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("text.txt", "utf8");
+    console.log(data);  
+});
+
+
  
-setTimeout(function(){
-         
-        console.log("timeout 100");
-}, 100);
- 
-displaySync(function(){console.log("without timeout")});
-console.log("Завершение работы программы");
+fs.appendFile("text.txt", "It's app on NodeJS ", function(error){
+    if(error) throw error; // если возникла ошибка
+                 
+    console.log("Запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("text.txt", "utf8");
+    console.log(data);  // выводим считанные данные
+});
+
+fs.readFile("text.txt", "utf8", 
+            function(error,data){
+                console.log("Асинхронное чтение файла");
+                if(error) throw error; 
+                console.log(data);  
+ });
+
+//fs.unlinkSync("text.txt");
+
+fs.unlink("text.txt", (err) => {
+  if (err) console.log(err); // если возникла ошибка    
+  else console.log("text.txt was deleted");
+});
