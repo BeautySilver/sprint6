19a94ce5145fd22e7a3d5dce0532d0dc6d9009a5
BeautySilver <gubar1234567@gmail.com>
2019-06-08 16:23:44 +0300

    Finish Assync

--- first_app.js
+++ first_app.js
@@ -1,36 +1,18 @@
-const http = require("http");
- 
-function display(data, callback){
- 
-    
-    let randInt = Math.random() * (10 - 1) + 1;
-    let err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
-     
-    setTimeout(function(){
-        callback(err, data);
-    }, 0);
+function displaySync(callback){
+    callback();
 }
-  console.log("Начало работы программы");
-
-displaySync("Обработка данных...",function (err, data){
  
-    if(err) throw err;
-    console.log(data);
-});
-
-
-let message = "Hello World!";
-http.createServer(function(request,response){
-     
-    console.log(message);
-    response.end(message);
-     
-}).listen(3000, "127.0.0.1",()=>{
-    console.log("Сервер начал прослушивание запросов");
-});
-
-function displaySync(data){
-    console.log(data);
-}
-  
+console.log("Начало работы программы");
+ 
+setTimeout(function(){
+         
+        console.log("timeout 500");
+}, 500);
+ 
+setTimeout(function(){
+         
+        console.log("timeout 100");
+}, 100);
+ 
+displaySync(function(){console.log("without timeout")});
 console.log("Завершение работы программы");
