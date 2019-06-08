9063d079c2ae1a773a565f2c0de4d8d863084365
BeautySilver <gubar1234567@gmail.com>
2019-06-08 22:00:15 +0300

    Create server

--- first_app.js
+++ first_app.js
@@ -1,10 +1,37 @@
-const fs = require("fs");
-const zlib = require("zlib");
-  
-let readableStream = fs.createReadStream("hello.txt", "utf8");
-  
-let writeableStream = fs.createWriteStream("hello.txt.gz");
-  
-let gzip = zlib.createGzip();
-  
-readableStream.pipe(gzip).pipe(writeableStream);
+const http = require("http");
+ 
+http.createServer(function(request, response){
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello Node.js</title>");
+    response.write("<meta charset=\"utf-8\" />");
+    response.write("</head>");
+    response.write("<body>");
+    if(request.url === "/home" || request.url === "/"){
+        response.write("<h2>Home</h2>");
+        response.write("<h2>Hello User</h2>")
+    }
+    else if(request.url == "/about"){
+        response.write("<h2>About</h2>");
+    }
+    else if(request.url == "/contact"){
+        response.write("<h2>Contacts</h2>");
+    }
+    else{
+        response.write("<h2>Not found</h2>");
+    }
+    response.write("</write>");
+    response.write("</html>");
+
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+    response.end();
+}).listen(3000);
+
+
+
