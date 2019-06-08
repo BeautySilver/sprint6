608e019f33e4401e9cf9cc295576b7f3ad7b6757
BeautySilver <gubar1234567@gmail.com>
2019-06-08 17:01:41 +0300

    Finish Stream

--- first_app.js
+++ first_app.js
@@ -1,52 +1,11 @@
-const EventEmitter = require("events");
-  
-let eventName = "greet";
+const fs = require("fs");
  
-class User extends EventEmitter {
-    sayHi(data) {
-        this.emit(eventName, data);
-    }
-}
+let writeableStream = fs.createWriteStream("hello.txt");
+writeableStream.write("Привет мир!");
+writeableStream.write("Продолжение записи \n");
+writeableStream.end("Завершение записи");
+let readableStream = fs.createReadStream("hello.txt", "utf8");
  
-let user = new User();
-// добавляем к объекту user обработку события "greet"
-user.on(eventName, function(data){
-    console.log(data);
+readableStream.on("data", function(chunk){ 
+    console.log(chunk);
 });
-  
-user.sayHi("I need only your close...");
-
-
-// const Emitter = require("events");
-
-// function User(){
-// }
-
-
-// let eventName = "greet";
-
-// util.inherits(User, Emitter);
-
-// User.prototype.sayHi = function(data){
-//     this.emit(eventName, data);
-// }
-// let user = new User();
-
-// user.on(eventName, function(data){
-//     console.log(data);
-// });
- 
-// user.sayHi("I need only your close...");
-// emitter.on(eventName, function(){
-//     console.log("Hello all!");
-// });
- 
-// emitter.on(eventName, function(){
-//     console.log("Hello!");
-// });
- 
-// emitter.on(eventName, function(data){
-//     console.log(data);
-// });
-
-// emitter.emit(eventName, "Hi");
