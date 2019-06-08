ba71ec9ad7e6c7df1521489201c52b3fde2ca807
BeautySilver <gubar1234567@gmail.com>
2019-06-08 16:11:14 +0300

    Finish NPM.

--- first_app.js
+++ first_app.js
@@ -1,10 +1,14 @@
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
+
+const express = require("express");
+
+const app = express();
 let name = process.argv[2];
 let age = process.argv[3];
  
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
 console.log("name: " + name);
 console.log("age: " + age);
+app.get("/", function(request, response){
+ 
+    response.end("Hello from Express!"); 
+});
+app.listen(3000);
