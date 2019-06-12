77cae587f056689e7be9c0207e104350dd6b72a3
BeautySilver <gubar1234567@gmail.com>
2019-06-12 14:43:20 +0300

    JSON AJAX

--- first_app.js
+++ first_app.js
@@ -1,25 +1,19 @@
 const express = require("express");
-
+  
 const app = express();
-const productRouter=express.Router();
- 
-productRouter.use("/create",function (request, response) {
-  response.send("Add product");
+
+const jsonParser = express.json();
+  
+app.post("/user", jsonParser, function (request, response) {
+    console.log(request.body);
+    if(!request.body) return response.sendStatus(400);
+     
+    response.json(request.body); 
 });
-productRouter.use("/:id",function (request, response) {
-  response.send(`product ${request.params.id}`);
+  
+app.get("/", function(request, response){
+      
+    response.sendFile(__dirname + "/index.html");
 });
-productRouter.use("/",function (request, response) {
-  response.send("products list");
-});
- 
-app.use("/products", productRouter);
- 
-app.use("/about", function (request, response) {
-  response.send("About");
-});
- 
-app.use("/", function (request, response) {
-  response.send("Main page");
-});
+  
 app.listen(3000);
