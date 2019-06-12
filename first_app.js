df39435aadec9750e637b5c0799789903ddaf3ca
BeautySilver <gubar1234567@gmail.com>
2019-06-12 13:58:08 +0300

    Body parser

--- first_app.js
+++ first_app.js
@@ -1,28 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
   
 const app = express();
-app.get("/", function(request, response){
-      
-    response.send("<h1>Главная страница</h1>");
-});
-app.use("/about", function(request, response){
-      
-    console.log(request.query);
-    let names = request.query.name;
-    let responseText = "<ul>";
-    for(let i=0; i < names.length; i++){
-        responseText += "<li>" + names[i] + "</li>";
-    }
-    responseText += "</ul>";
-    response.send(responseText);
-});
-app.use("/contact", function(request, response){
-      
-    console.log(request.query);
-    let id = request.query.user.id;
-    let name = request.query.user.name;
-     
-    response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
-});
+  
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = bodyParser.urlencoded({extended: false});
  
+app.get("/register", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/register.html");
+});
+app.post("/register", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+  
+app.get("/", function(request, response){
+    response.send("Главная страница");
+});
+  
 app.listen(3000);
