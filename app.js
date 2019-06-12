6ee586171ff5669dbca964adffee3b9bf2fb7fa2
BeautySilver <gubar1234567@gmail.com>
2019-06-12 18:03:11 +0300

    Manage Mongo

--- app.js
+++ app.js
@@ -4,35 +4,28 @@ const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
  
 mongoClient.connect(function(err, client){
-      
-    const db = client.db("usersdb");
-    const collection = db.collection("users");
- 
+     
     if(err) return console.log(err);
       
-    collection.find({name: "Tom", age:23}).toArray(function(err, results){
-                 
-        console.log(results);
+    const db = client.db("usersdb");
+    db.collection("users").deleteMany({name: "Tom", age:23}, function(err, result){
+              
+        console.log(result);
+        client.close();
+    });
+     db.collection("users").deleteOne({name: "Bob"}, function(err, result){
+              
+        console.log(result);
+        client.close();
+    });
+      db.collection("users").findOneAndDelete({age: 45}, function(err, result){
+              
+        console.log(result);
+        client.close();
+    });
+      db.collection("users").drop(function(err, result){
+              
+        console.log(result);
         client.close();
     });
 });
-
-
-// const MongoClient = require("mongodb").MongoClient;
-   
-// const url = "mongodb://localhost:27017/";
-// const mongoClient = new MongoClient(url, { useNewUrlParser: true });
- 
-// let users = [{name: "Bob", age: 34} , {name: "Alice", age: 21}, {name: "Tom", age: 45}];
- 
-// mongoClient.connect(function(err, client){
-      
-//     const db = client.db("usersdb");
-//     const collection = db.collection("users");
-     
-//     collection.insertMany(users, function(err, results){
-              
-//         console.log(results);
-//         client.close();
-//     });
-// });
