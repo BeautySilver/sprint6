7531fc976804401a955dcfea8c0d9260853d509f
BeautySilver <gubar1234567@gmail.com>
2019-06-12 18:08:01 +0300

    Update Mongo

--- app.js
+++ app.js
@@ -8,24 +8,14 @@ mongoClient.connect(function(err, client){
     if(err) return console.log(err);
       
     const db = client.db("usersdb");
-    db.collection("users").deleteMany({name: "Tom", age:23}, function(err, result){
-              
-        console.log(result);
-        client.close();
-    });
-     db.collection("users").deleteOne({name: "Bob"}, function(err, result){
-              
-        console.log(result);
-        client.close();
-    });
-      db.collection("users").findOneAndDelete({age: 45}, function(err, result){
-              
-        console.log(result);
-        client.close();
-    });
-      db.collection("users").drop(function(err, result){
-              
-        console.log(result);
-        client.close();
-    });
+    const col = db.collection("users");
+    col.updateOne(
+        {name: "Tom"}, 
+        { $set: {name: "Tom Junior", age:33}},
+        function(err, result){
+                  
+            console.log(result);
+            client.close();
+        }
+    );
 });
