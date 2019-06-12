64cc9041a0463217f51c89185bc2bcbe93f70251
BeautySilver <gubar1234567@gmail.com>
2019-06-12 15:45:24 +0300

    ejs

--- first_app.js
+++ first_app.js
@@ -1,39 +1,20 @@
 const express = require("express");
-const hbs = require("hbs");
  
 const app = express();
  
-hbs.registerHelper("getTime", function(){
+app.set("view engine", "ejs");
+ 
+app.use("/contact", function(request, response){
      
-    var myDate = new Date();
-    var hour = myDate.getHours();
-    var minute = myDate.getMinutes();
-    var second = myDate.getSeconds();
-    if (minute < 10) {
-        minute = "0" + minute;
-    }
-    if (second < 10) {
-        second = "0" + second;
-    }
-    return "Текущее время: " + hour + ":" + minute + ":" + second;
-});
- 
-hbs.registerHelper("createStringList", function(array){
-     
-    var result="";
-    for(var i=0; i<array.length; i++){
-        result +="<li>" + array[i] + "</li>";
-    }
-    return new hbs.SafeString("<ul>" + result + "</ul>");
-});
- 
- 
-app.set("view engine", "hbs");
- 
-app.get("/", function(request, response){
-     
-    response.render("home.hbs", { 
-        fruit: [ "apple", "lemon", "banana", "grape"]
+    response.render("contact", {
+        title: "Мои контакты",
+        emailsVisible: true,
+        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
+        phone: "+1234567890"
     });
+});
+app.use("/", function(request, response){
+     
+    response.send("Главная страница");
 });
 app.listen(3000);
