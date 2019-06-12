c7c9675e5518050b5f176ac23ff7077cfa0cf685
BeautySilver <gubar1234567@gmail.com>
2019-06-12 21:12:58 +0300

    promise

--- app.js
+++ app.js
@@ -5,25 +5,14 @@ const connection = mysql.createConnection({
   user: "root",
   database: "pultickbot",
   password: "S233kaas17102001"
-});
- connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
- connection.query("SELECT * FROM clients",
-  function(err, results, fields) {
-    console.log(err);
-    console.log(results); 
-    console.log(fields); 
-});
+}).promise();
 
- connection.end(function(err) {
-  if (err) {
-    return console.log("Ошибка: " + err.message);
-  }
-  console.log("Подключение закрыто");
-});
+ connection.query("SELECT * FROM clients")
+         .then(([rows, fields]) =>{
+            console.log(rows);
+          })
+          .catch(err =>{
+            console.log(err);
+          });
+
+ 
