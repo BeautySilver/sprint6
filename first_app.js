3d7c32e1d0f4303d92b7f267c34aed503501dc35
BeautySilver <gubar1234567@gmail.com>
2019-06-12 15:54:43 +0300

    Finish Express

--- first_app.js
+++ first_app.js
@@ -1,20 +1,119 @@
-const express = require("express");
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
  
-const app = express();
+var app = express();
+var jsonParser = bodyParser.json();
  
-app.set("view engine", "ejs");
- 
-app.use("/contact", function(request, response){
-     
-    response.render("contact", {
-        title: "Мои контакты",
-        emailsVisible: true,
-        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
-        phone: "+1234567890"
-    });
+app.use(express.static(__dirname + "/public"));
+
+app.get("/api/users", function(req, res){
+      
+    var content = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(content);
+    res.send(users);
 });
-app.use("/", function(request, response){
-     
-    response.send("Главная страница");
+
+app.get("/api/users/:id", function(req, res){
+      
+    var id = req.params.id; 
+    var content = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(content);
+    var user = null;
+    
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==id){
+            user = users[i];
+            break;
+        }
+    }
+   
+    if(user){
+        res.send(user);
+    }
+    else{
+        res.status(404).send();
+    }
 });
-app.listen(3000);
+
+app.post("/api/users", jsonParser, function (req, res) {
+     
+    if(!req.body) return res.sendStatus(400);
+     
+    var userName = req.body.name;
+    var userAge = req.body.age;
+    var user = {name: userName, age: userAge};
+     
+    var data = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(data);
+   
+    var id = Math.max.apply(Math,users.map(function(o){return o.id;}))
+    
+    user.id = id+1;
+    
+    users.push(user);
+    var data = JSON.stringify(users);
+   
+    fs.writeFileSync("users.json", data);
+    res.send(user);
+});
+ 
+app.delete("/api/users/:id", function(req, res){
+      
+    var id = req.params.id;
+    var data = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(data);
+    var index = -1;
+    
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==id){
+            index=i;
+            break;
+        }
+    }
+    if(index > -1){
+       
+        var user = users.splice(index, 1)[0];
+        var data = JSON.stringify(users);
+        fs.writeFileSync("users.json", data);
+      
+        res.send(user);
+    }
+    else{
+        res.status(404).send();
+    }
+});
+
+app.put("/api/users", jsonParser, function(req, res){
+      
+    if(!req.body) return res.sendStatus(400);
+     
+    var userId = req.body.id;
+    var userName = req.body.name;
+    var userAge = req.body.age;
+     
+    var data = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(data);
+    var user;
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==userId){
+            user = users[i];
+            break;
+        }
+    }
+    
+    if(user){
+        user.age = userAge;
+        user.name = userName;
+        var data = JSON.stringify(users);
+        fs.writeFileSync("users.json", data);
+        res.send(user);
+    }
+    else{
+        res.status(404).send(user);
+    }
+});
+  
+app.listen(3000, function(){
+    console.log("Сервер ожидает подключения...");
+});
