8d116a0aeb066670dc15d82f5245817143b8354c
BeautySilver <gubar1234567@gmail.com>
2019-06-12 21:30:49 +0300

    Express and MySQL

--- app.js
+++ app.js
@@ -1,18 +1,79 @@
 const mysql = require("mysql2");
-  
-const connection = mysql.createConnection({
+const express = require("express");
+const bodyParser = require("body-parser");
+ 
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+ 
+const pool = mysql.createPool({
+  connectionLimit: 5,
   host: "localhost",
   user: "root",
-  database: "pultickbot",
+  database: "users",
   password: "S233kaas17102001"
-}).promise();
-
- connection.query("SELECT * FROM clients")
-         .then(([rows, fields]) =>{
-            console.log(rows);
-          })
-          .catch(err =>{
-            console.log(err);
-          });
-
+});
  
+app.set("view engine", "hbs");
+ 
+// получение списка пользователей
+app.get("/", function(req, res){
+    pool.query("SELECT * FROM user", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index.hbs", {
+          users: data
+      });
+    });
+});
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create.hbs");
+});
+// получаем отправленные данные и добавляем их в БД 
+app.post("/create", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const age = req.body.age;
+    pool.query("INSERT INTO user (name, age) VALUES (?,?)", [name, age], function(err, data) {
+      if(err) return console.log(err);
+      res.redirect("/");
+    });
+});
+ 
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  pool.query("SELECT * FROM user WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit.hbs", {
+        user: data[0]
+    });
+  });
+});
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+  const name = req.body.name;
+  const age = req.body.age;
+  const id = req.body.id;
+   
+  pool.query("UPDATE user SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
