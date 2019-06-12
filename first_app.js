e3d03bb9f49e468e7de8f06a8a9b930b26ef88f3
BeautySilver <gubar1234567@gmail.com>
2019-06-12 13:15:49 +0300

    Middleware

--- first_app.js
+++ first_app.js
@@ -1,16 +1,46 @@
 const express = require("express");
+const fs = require("fs");
  
 const app = express();
+app.use(function(request, response, next){
+     
+    let now = new Date();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let data = `${hour}:${minutes}:${seconds} ${request.method} ${request.url} ${request.get("user-agent")}`;
+    console.log(data);
+    fs.appendFile("server.log", data + "\n", function(){});
+    next();
+});
+ 
 app.get("/", function(request, response){
-     
-    response.send("<h1>Main page</h1>");
-});
-app.get("/about", function(request, response){
-     
-    response.send("<h1>About</h1>");
-});
-app.get("/contact", function(request, response){
-     
-    response.send("<h1>Contacts</h1>");
+    response.send("Hello");
 });
 app.listen(3000);
+
+
+
+
+
+
+// const express = require("express");
+ 
+// const app = express();
+// app.use(function(request, response, next){
+     
+//     console.log("Middleware 1");
+//     next();
+// });
+// app.use("/about" function(request, response, next){
+     
+//     console.log("Middleware 2");
+//     response.send("Middleware 2");
+// });
+ 
+// app.get("/", function(request, response){
+     
+//     console.log("Route /");
+//     response.send("Hello");
+// });
+// app.listen(3000);
