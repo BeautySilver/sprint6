332c3998172275f13a8ee8c1591a0b72081fc7dc
BeautySilver <gubar1234567@gmail.com>
2019-06-12 13:11:39 +0300

    Start work with Express

--- first_app.js
+++ first_app.js
@@ -1,13 +1,16 @@
-const http = require("http");
-const fs =require("fs");
-
-http.createServer(function(request, response){
-   fs.readFile("index.html","utf8", function(error, data){
-   	  let message ="Learn Node.js";
-   	  let header = "Main page";
-   	  data = data.replace("{header}",header).replace("{message}", message);
-   	  response.end(data);
-   })
-}).listen(3000);
-
-
+const express = require("express");
+ 
+const app = express();
+app.get("/", function(request, response){
+     
+    response.send("<h1>Main page</h1>");
+});
+app.get("/about", function(request, response){
+     
+    response.send("<h1>About</h1>");
+});
+app.get("/contact", function(request, response){
+     
+    response.send("<h1>Contacts</h1>");
+});
+app.listen(3000);
