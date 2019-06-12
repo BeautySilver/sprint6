464ab2cfd932837c47f88c78d9ce2001f9f5c002
BeautySilver <gubar1234567@gmail.com>
2019-06-12 17:02:55 +0300

    Describe

--- operations.test.js
+++ operations.test.js
@@ -1,9 +1,17 @@
 var assert = require("assert");
 var operations = require("./operations");
  
-it("should multiply two numbers", function(){
-     
-    var expected = 15;
-    var result = operations.multiply(3, 5);
-    assert.notEqual(result, expected);
+describe("Operation Tests", function(){
+    it("should multiply two numbers", function(){
+         
+        var expected = 15;
+        var result = operations.multiply(3, 5);
+        assert.equal(result, expected);
+    });
+    it("should add two numbers", function(){
+         
+        var expected = 16;
+        var result = operations.add(9, 7);
+        assert.equal(result, expected);
+    });
 });
