54ec7c67cbb8312643f3bd13bbc51730d3eee81a
BeautySilver <gubar1234567@gmail.com>
2019-06-12 13:32:24 +0300

    Static files

--- first_app.js
+++ first_app.js
@@ -1,32 +1,12 @@
 const express = require("express");
+ 
 const app = express();
  
-app.use(function (request, response) {
-  response.sendFile(__dirname + "/index.html");
-  
+app.use(express.static(__dirname + "/public"));
+ 
+app.use("/", function(request, response){
+     
+    response.send("<h1>Main page</h1>");
 });
- app.use("/home/foo/bar",function (request, response) {
-  response.sendStatus(404)
-});
+ 
 app.listen(3000);
-
-
-
-// const express = require("express");
-// const app = express();
- 
-// app.use(function (request, response) {
-//   response.send(`<!DOCTYPE html>
-//   <html>
-//   <head>
-//       <title>Главная</title>
-//       <meta charset="utf-8" />
-//   </head>
-//   <body>
-//       <h1>Главная страница</h1>
-//       <h3>Привет, Express</h3>
-//   </body>
-//   <html>`);
-// });
- 
-// app.listen(3000);
