0b2ec91945add5baf1888e109f9bd18a70ae52b1
BeautySilver <gubar1234567@gmail.com>
2019-06-12 12:22:00 +0300

    Sending files

--- first_app.js
+++ first_app.js
@@ -1,37 +1,49 @@
 const http = require("http");
- 
+const fs = require("fs");
+  
 http.createServer(function(request, response){
-    response.setHeader("Content-Type", "text/html; charset=utf-8;");
-    response.write("<!DOCTYPE html>");
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello Node.js</title>");
-    response.write("<meta charset=\"utf-8\" />");
-    response.write("</head>");
-    response.write("<body>");
-    if(request.url === "/home" || request.url === "/"){
-        response.write("<h2>Home</h2>");
-        response.write("<h2>Hello User</h2>")
-    }
-    else if(request.url == "/about"){
-        response.write("<h2>About</h2>");
-    }
-    else if(request.url == "/contact"){
-        response.write("<h2>Contacts</h2>");
-    }
-    else{
-        response.write("<h2>Not found</h2>");
-    }
-    response.write("</write>");
-    response.write("</html>");
+      
+    console.log(`Запрошенный адрес: ${request.url}`);
+ 
+    const filePath = request.url.substr(1);
+    fs.readFile(filePath, function(error, data){
+              
+        if(error){
+                  
+            response.statusCode = 404;
+            response.end("Resourse not found!");
+        }   
+        else{
+            response.end(data);
+        }
+    });
+}).listen(3000, function(){
+    console.log("Server started at 3000");
+});
 
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
-    response.end();
-}).listen(3000);
 
+
+// const http = require("http");
+// const fs = require("fs");
+  
+// http.createServer(function(request, response){
+      
+//     console.log(`Запрошенный адрес: ${request.url}`);
+//     
+//     const filePath = request.url.substr(1);
+//  
+//     fs.access(filePath, fs.constants.R_OK, err => {
+//        
+//         if(err){
+//             response.statusCode = 404;
+//             response.end("Resourse not found!");
+//         }
+//         else{
+//             fs.createReadStream(filePath).pipe(response);
+//         }
+//       });
+// }).listen(3000, function(){
+//     console.log("Server started at 3000");
+// });
 
 
