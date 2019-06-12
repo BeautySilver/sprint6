9bb28faf86048e73a5067217d0e780cfb589db2c
BeautySilver <gubar1234567@gmail.com>
2019-06-12 15:31:25 +0300

    Helper hbs

--- first_app.js
+++ first_app.js
@@ -1,31 +1,39 @@
 const express = require("express");
-const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
+ 
 const app = express();
-  
-// устанавливаем настройки для файлов layout
-app.engine("hbs", expressHbs(
-    {
-        layoutsDir: "views/layoults", 
-        defaultLayout: "layoult",
-        extname: "hbs"
+ 
+hbs.registerHelper("getTime", function(){
+     
+    var myDate = new Date();
+    var hour = myDate.getHours();
+    var minute = myDate.getMinutes();
+    var second = myDate.getSeconds();
+    if (minute < 10) {
+        minute = "0" + minute;
     }
-))
+    if (second < 10) {
+        second = "0" + second;
+    }
+    return "Текущее время: " + hour + ":" + minute + ":" + second;
+});
+ 
+hbs.registerHelper("createStringList", function(array){
+     
+    var result="";
+    for(var i=0; i<array.length; i++){
+        result +="<li>" + array[i] + "</li>";
+    }
+    return new hbs.SafeString("<ul>" + result + "</ul>");
+});
+ 
+ 
 app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
  
-app.use("/contact", function(request, response){
-      
-    response.render("contact", {
-        title: "Мои контакты",
-        emailsVisible: true,
-        emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
-        phone: "+1234567890"
+app.get("/", function(request, response){
+     
+    response.render("home.hbs", { 
+        fruit: [ "apple", "lemon", "banana", "grape"]
     });
-}); 
- 
-app.use("/", function(request, response){
-      
-    response.render("home.hbs");
 });
 app.listen(3000);
